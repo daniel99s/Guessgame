cef5981dbb6707edb8568d56260ff9cf38ad79bc
Luis Soto <luis.daniel.soto.escalante@uabc.edu.mx>
2021-10-10 21:28:37 -0700

    juego de  la maquina adivina mi numero terminado

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-import random 
-def humanguess():
-    number=random.randrange(0,10)
-    print (number)
-
-    num =-1
-
-    num=int(input("Ingresa un numero del 1 al 10"))
-
-    while num!=number:
-        if number==num:
-            print(f"Felicidades ganaste{num}")
-            
-
-        elif num>number:
-            print(f"el numero que eligio es mayor{num}")
-
+import random
+def humanGuess ():
+    number = random.randrange(0,10)
+    print(number)
+    op = -1
+    while(number != op):
+        op = int(input("Introduce un numero: "))
+        if (number == op):
+            print("Felicidades ganaste, el numero era {}".format(number))
         else:
-            print(f"el numero que aligio es menor{num}")
+            if(number > op): 
+                print("Te falto")
+            else:
+                print("Te pasaste")
 
-humanguess()
+def computerGuess():
+    NumMenor=0
+    NumMayor=1000
+    number = random.randrange(NumMenor,NumMayor) 
+    bandera = False 
+    
+    while(bandera == False):
+        print (number)
+        print("Este es tu numero?")
+        opc= input("Si | No:  ").lower()
+        if( opc == "no"):
+            print("Tu numero es mayor o menor al mio?")
+            opc2 = input("Mayor | Menor: ").lower()
+            if (opc2 == "menor"): 
+                NumMayor=number
+                Num= random.randrange(NumMenor,NumMayor)
+                number=Num
+            else: 
+                  NumMenor=number
+                  Num = random.randrange(NumMenor,NumMayor)
+                  number=Num
+        else:
+            bandera = True
+
+    
+computerGuess()
